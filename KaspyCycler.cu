372cdf60bc88ff1a744af3138df3732ec2b7dcb0
pprd <pprd@evgenij-kulikovs-macbook-pro.local>
2016-05-31 07:50:27 +0300

    dfgd

--- KaspyCycler.cu
+++ KaspyCycler.cu
@@ -484,6 +484,9 @@ void KaspyCycler::makeWsurf(float ro_ratio)
 	
 	
 	surf_and_flux_1<<<numSquareBlocks, threadsPerSquareBlock>>>(ftim, ro_ratio,  g_fbu,  g_ffu,  g_fbv,  g_ffv,  g_dum,  g_dvm,  g_d,  g_wusurf,  g_wvsurf,  g_fluxua,  g_fluxva,  g_dx,  g_dy,  g_ua,  g_va,  g_fxf,  g_fyf, g_fxb,  g_fyb);
+	
+	cudaDeviceSynchronize();
+	
 	//setbuf(stdout,NULL);
 	//printf("before call\n");
 	//setbuf(stdout,NULL);
