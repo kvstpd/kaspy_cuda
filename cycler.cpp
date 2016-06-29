056a78d38ffcb7f1ca995e05a9057848ee749bfc
pprd <pprd@evgenij-kulikovs-macbook-pro.local>
2016-06-29 23:47:32 +0300

    fdfdf

--- cycler.cpp
+++ cycler.cpp
@@ -206,14 +206,14 @@ void _i_cycler_init(int * icycler, float * vars_marker, double * arrays_marker, 
 extern "C" void CYCLER_CREATE(int * icycler, float * vars_marker, double * arrays_marker, double * ffloats_marker,
                              int * wind_marker, float * press)
 {
-    _i_cycler_init(icycler, vars_marker, arrays_marker, ffloats_marker, wind_marker);
+    _i_cycler_init(icycler, vars_marker, arrays_marker, ffloats_marker, wind_marker, press);
 }
 
 // GFortran Unix naming
 extern "C" void cycler_create_(int * icycler, float * vars_marker, double * arrays_marker, double * ffloats_marker,
                               int * wind_marker)
 {
-    _i_cycler_init(icycler, vars_marker, arrays_marker, ffloats_marker, wind_marker);
+    _i_cycler_init(icycler, vars_marker, arrays_marker, ffloats_marker, wind_marker, 0);
 }
 
 
